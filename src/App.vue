3a22f09cec1132843c5c45e1b588d3461c094287
jeffery_9 <jeffery_9@163.com>
2024-01-31 16:18:06 +0800

    路由配置

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,9 @@
 <script setup lang="ts">
-import { ref } from 'vue'
 
-const value1 = ref(true)
-const value2 = ref(true)
 </script>
 
 <template>
-  <el-switch v-model="value1" />
-  <el-switch
-    v-model="value2"
-    class="ml-2"
-    style="--el-switch-on-color: #13ce66; --el-switch-off-color: #ff4949"
-  />
+  <RouterView/>
 </template>
 
 <style scoped>
--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,17 @@ import { createRouter, createWebHistory } from 'vue-router'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    {
+      path: '/',
+      component: () => import('@/views/Main.vue'),
+      children: [
+        {
+          path: '/',
+          name: 'home',
+          component: () => import('@/views/home/home.vue')
+        }
+      ]
+    }
   ]
 })
 
