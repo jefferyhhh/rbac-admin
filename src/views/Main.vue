9ee53d080fdcee0739de92dc045b30f5bb32be08
jeffery_9 <jeffery_9@163.com>
2024-02-11 21:56:59 +0800

    新增main.vue

---
+++ src/views/Main.vue
@@ -0,0 +1,36 @@
+<script lang="ts" setup>
+import CommonHeader from '../components/CommonHeader.vue'
+import CommonAside from '../components/CommonAside.vue'
+</script>
+
+<template>
+  <div class="common-layout">
+    <el-container>
+      <CommonAside />
+      <el-container class="r-container">
+        <CommonHeader />
+        <el-main>
+          <RouterView />
+        </el-main>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+<style lang="less" scoped>
+.el-container {
+  flex-wrap: wrap;
+  align-items: flex-start;
+}
+
+.common-layout {
+  height: 100%;
+  & > .el-container {
+    height: 100%;
+    & > .el-aside {
+      height: 100%;
+      background: #545c64;
+    }
+  }
+}
+</style>
