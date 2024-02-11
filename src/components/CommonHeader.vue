28b34ecbebad7608ca473d4405e5dce6ec97e20f
jeffery_9 <jeffery_9@163.com>
2024-02-11 21:58:30 +0800

    新增commonHeader.vue

---
+++ src/components/CommonHeader.vue
@@ -0,0 +1,61 @@
+<script lang="ts" setup>
+//vite动态url，处理静态资源
+const getImgScr = (avatar: string) => {
+  return new URL(`../assets/${avatar}.png`, import.meta.url).href
+}
+</script>
+
+<template>
+  <el-header>
+    <!-- 图标 -->
+    <div class="l-content">
+      <el-button size="small">
+        <el-icon>
+          <Menu />
+        </el-icon>
+      </el-button>
+      <h3>首页</h3>
+    </div>
+    <div class="r-content">
+      <el-dropdown :hide-on-click="false">
+        <span class="el-dropdown-link">
+          <img class="user" :src="getImgScr('avatar')" />
+        </span>
+        <template #dropdown>
+          <el-dropdown-menu>
+            <el-dropdown-item>个人中心</el-dropdown-item>
+            <el-dropdown-item><el-text class="mx-1" type="danger">退出</el-text></el-dropdown-item>
+          </el-dropdown-menu>
+        </template>
+      </el-dropdown>
+    </div>
+  </el-header>
+</template>
+
+<style lang="less" scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  background: #333;
+}
+.r-content {
+  .user {
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+  }
+}
+
+.l-content {
+  display: flex;
+  align-items: center;
+  .el-button {
+    margin-right: 20px;
+  }
+  h3 {
+    color: #fff;
+  }
+}
+</style>
