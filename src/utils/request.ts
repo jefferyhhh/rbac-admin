c608ed0fc5b03a12718142c8aa78ef06337e97f7
jeffery_9 <jeffery_9@163.com>
2024-07-16 14:25:19 +0800

    完善axios封装

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,6 +1,9 @@
 import axios from 'axios'
-import type { AxiosInstance, AxiosError, AxiosRequestConfig } from 'axios'
-import { ElMessage } from 'element-plus'
+import storage from 'store'
+import router from '@/router'
+import { getNewToken } from '@/api/user'
+import { save_token } from '@/utils/storage'
+import type { AxiosInstance, AxiosError, AxiosRequestConfig, AxiosResponse } from 'axios'
 
 /* 服务器返回数据的的类型，根据接口文档确定 */
 export interface Result<T = any> {
@@ -14,27 +17,71 @@ const service: AxiosInstance = axios.create({
   timeout: 5000
 })
 
-// 请求拦截器
-service.interceptors.request.use(
-  (config) => {
-    return config
-  },
-  (error: AxiosError) => {
-    ElMessage.error(error.message)
+// 异常拦截处理器
+const errorHandler = async (error: AxiosError) => {
+  if (!error.response) {
+    return Promise.reject(error) //未接收到服务器响应，网络等原因
+  }
+  const errRes: AxiosResponse = error.response
+  const { code, message } = errRes.data
+  const access_token = storage.get('Access-Token') //获取 access_token
+  if (code === 403) {
+    storage.remove('Access-Token')
+    storage.remove('Refresh-Token')
+
+    ElNotification({
+      title: '错误',
+      message: message,
+      type: 'error'
+    })
+    router.push('/login')
     return Promise.reject(error)
   }
-)
+  if (code === 401) {
+    if (!access_token) {
+      ElNotification({
+        title: '错误',
+        message: message,
+        type: 'error'
+      })
+      router.push('/login')
+      return Promise.reject(error)
+    }
+    // 刷新token
+    const refresh_token = storage.get('Refresh-Token')
+    storage.remove('Access-Token')
+    storage.remove('Refresh-Token')
+    return getNewToken({ refresh_token: refresh_token }).then((response) => {
+      const { access_token, refresh_token, expires_in } = response.data
+      save_token(access_token, refresh_token, expires_in)
+      //重新发起刚刚错误的请求
+      return service(errRes.config).then((response) => response)
+    })
+  }
+}
+// 请求拦截器
+service.interceptors.request.use((config) => {
+  const token = storage.get('Access-Token')
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}` //携带token
+  }
+  return config
+}, errorHandler)
 
 // 响应拦截器
-service.interceptors.response.use(
-  (response) => {
-    return response.data
-  },
-  (error: AxiosError) => {
-    // 错误抛出
-    return Promise.reject(error)
+service.interceptors.response.use((response) => {
+  const { code, message, data } = response.data
+  if (code !== 200) {
+    ElNotification({
+      title: '错误',
+      message: message,
+      type: 'error'
+    })
+    console.log(message, data, 1, '错误-响应')
+    return Promise.reject(response)
   }
-)
+  return response.data
+}, errorHandler)
 
 //导出封装的请求方法
 export const http = {
