7645a2bcdb4ff43f8f860cbde98001dbe971e92d
jeffery_9 <jeffery_9@163.com>
2024-07-30 10:46:27 +0800

    杂项

--- src/views/Page2.vue
+++
@@ -1,7 +0,0 @@
-<script lang="ts" setup></script>
-
-<template>
-  <div>Page2.vue</div>
-</template>
-
-<style></style>
--- src/views/login/types.ts
+++ src/views/login/types.ts
@@ -3,11 +3,9 @@ export interface loginFormType {
   password: string
   captcha: string
   captcha_key: string
-  remember: boolean
 }
 
 export interface captchaStateType {
-  enable: boolean
   key: string
   img_base: string
 }
