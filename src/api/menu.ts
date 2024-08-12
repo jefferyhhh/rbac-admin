fed8eb40a7d5f0f597a0c168d8af482282ba91f2
jeffery_9 <jeffery_9@163.com>
2024-08-12 17:09:28 +0800

    新增菜单管理相关接口，增删改

--- src/api/menu.ts
+++ src/api/menu.ts
@@ -1,7 +1,26 @@
 import { http } from '@/utils/request'
-// import service from '@/utils/request'
+import service from '@/utils/request'
 import type { Result } from '@/utils/request'
 
 export function getMenuList() {
   return http.get<Result<MenuItem[]>>('/api/system/menu/list')
+}
+export function createMenu(body: MenuItem) {
+  return http.post<Result>('/api/system/menu/create', body)
+}
+export function updateMenu(body: MenuItem) {
+  return http.post<Result>('/api/system/menu/update', body)
+}
+export function deleteMenu(params: { id: number }): Promise<Result> {
+  return service({
+    url: '/api/system/menu/delete',
+    method: 'post',
+    params: params
+  })
+}
+export function getMenuOptions() {
+  return http.get<Result<MenuItem[]>>('/api/system/menu/options')
+}
+export function getMenuDetail(id: number) {
+  return http.get<Result<MenuItem>>('/api/system/menu/detail' + '?id=' + id)
 }
---
+++ src/views/dashboard/workplace.vue
@@ -0,0 +1,7 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div>workplace组件</div>
+</template>
+
+<style></style>
