3110c8b90b1dc1d2a165b9482163e5d4ac136576
jeffery_9 <jeffery_9@163.com>
2024-09-12 10:24:22 +0800

    杂项

--- components.d.ts
+++ components.d.ts
@@ -49,7 +49,6 @@ declare module 'vue' {
     ElScrollbar: typeof import('element-plus/es')['ElScrollbar']
     ElSelect: typeof import('element-plus/es')['ElSelect']
     ElSubMenu: typeof import('element-plus/es')['ElSubMenu']
-    ElSwitch: typeof import('element-plus/es')['ElSwitch']
     ElTable: typeof import('element-plus/es')['ElTable']
     ElTableColumn: typeof import('element-plus/es')['ElTableColumn']
     ElTabPane: typeof import('element-plus/es')['ElTabPane']
